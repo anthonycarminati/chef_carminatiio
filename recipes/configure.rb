803c9fa5ba5e0b4fa3282e272b185af3decc5d11
Anthony Carminati <anthony@carminati.io>
2015-12-11 15:44:15 -0600

    added pip requirements integration

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -14,19 +14,19 @@ service "codedeploy-agent" do
   action [:enable, :start]
 end
 
-## ----------
-## Installs Python packages via python-pip
-## ----------
-# node[:pip_python_packages].each_pair do |pkg, version|
-#     execute "install-#{pkg}" do
-#         command "pip install #{pkg}==#{version}"
-#         not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
-#     end
-# end
+# ----------
+# Installs Python packages via python-pip
+# ----------
+node[:pip_python_packages].each_pair do |pkg, version|
+    execute "install-#{pkg}" do
+        command "pip install #{pkg}==#{version}"
+        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+    end
+end
 
-# node[:pip_python_packages].each do |pkg|
-#     execute "install-#{pkg}" do
-#         command "pip install #{pkg}"
-# #        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
-#     end
-# end
+node[:pip_python_packages].each do |pkg|
+    execute "install-#{pkg}" do
+        command "pip install #{pkg}"
+#        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
+    end
+end
